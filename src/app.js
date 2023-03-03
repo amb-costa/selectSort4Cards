cd8d9e24ebbd476ee60ff9a88f1a2e8abe2176aa
beinganidiot <ocaranzacosta@gmail.com>
2023-03-03 23:41:28 +0000

    minor CSS changes, tests for bubbleSort implementation

--- src/app.js
+++ src/app.js
@@ -2,11 +2,6 @@
 import "bootstrap";
 import "./style.css";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
-
 //functions to generate random numbers and suits
 function randomNumber() {
   let nmb = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
@@ -53,3 +48,39 @@ drawBtn.addEventListener("click", () => {
     cardSpace.append(oneCard);
   }
 });
+
+//listener for sorting button
+sortBtn.addEventListener("click", () => {
+  //obtaining every DIV using class selector
+  let rryUnsort = document.getElementsByClassName("card");
+  let numsUnsort = [];
+  //obtaining innerHTML/number from every card
+  for (let el of rryUnsort) {
+    numsUnsort.push(el.innerHTML);
+  }
+  console.log(rryUnsort);
+  console.log(numsUnsort);
+});
+
+//bubbleSort function adapted fron 4Geeks lesson
+//added aux object to record bubble log
+function bubbleSort(array) {
+  let wall = array.length - 1; //we start the wall at the end of the array
+  let auxObj = {};
+  while (wall > 0) {
+    let index = 0;
+    while (index < wall) {
+      let auxIndex = index;
+      //compare the adjacent positions, if the right one is bigger, we have to swap
+      if (array[index] > arr[index + 1]) {
+        let aux = array[index];
+        array[index] = array[index + 1];
+        array[index + 1] = aux;
+        auxObj[auxIndex] = array;
+      }
+      index++;
+    }
+    wall--; //decrease the wall for optimization
+  }
+  return array;
+}
