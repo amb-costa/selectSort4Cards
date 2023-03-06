f9f3fd17d41a60fabd5835688ecd761429b2c359
beinganidiot <ocaranzacosta@gmail.com>
2023-03-06 02:35:58 +0000

    finished sort, focus on non number characters

--- src/app.js
+++ src/app.js
@@ -44,43 +44,77 @@ drawBtn.addEventListener("click", () => {
     oneCard.classList.add("card");
     oneCard.classList.add(randomSuit());
     oneCard.innerHTML = randomNumber();
-    console.log(oneCard);
     cardSpace.append(oneCard);
   }
 });
 
 //listener for sorting button
 sortBtn.addEventListener("click", () => {
-  //obtaining every DIV using class selector
-  let rryUnsort = document.getElementsByClassName("card");
+  //obtaining every card DIV using class selector
+  let classUnsort = Array.from(document.getElementsByClassName("card"));
   let numsUnsort = [];
   //obtaining innerHTML/number from every card
-  for (let el of rryUnsort) {
-    numsUnsort.push(el.innerHTML);
-  }
-  console.log(rryUnsort);
-  console.log(numsUnsort);
-});
-
-//bubbleSort function adapted fron 4Geeks lesson
-//added aux object to record bubble log
-function bubbleSort(array) {
-  let wall = array.length - 1; //we start the wall at the end of the array
-  let auxObj = {};
-  while (wall > 0) {
-    let index = 0;
-    while (index < wall) {
-      let auxIndex = index;
-      //compare the adjacent positions, if the right one is bigger, we have to swap
-      if (array[index] > arr[index + 1]) {
-        let aux = array[index];
-        array[index] = array[index + 1];
-        array[index + 1] = aux;
-        auxObj[auxIndex] = array;
-      }
-      index++;
+  //in case of A J Q K, replace for number
+  for (let el of classUnsort) {
+    const num = el.innerHTML;
+    switch (num) {
+      case "A":
+        numsUnsort.push("1");
+        break;
+      case "J":
+        numsUnsort.push("11");
+        break;
+      case "Q":
+        numsUnsort.push("12");
+        break;
+      case "K":
+        numsUnsort.push("13");
+        break;
+      default:
+        numsUnsort.push(num);
     }
-    wall--; //decrease the wall for optimization
   }
-  return array;
-}
+  //adapting bubbleSort function from 4Geeks lesson
+  //sorting number array, applying changes to class array at the same time
+  //both arrays have the same length
+  let wall = numsUnsort.length - 1; //wall at end of array
+  while (wall > 0) {
+    let i = 0;
+    while (i < wall) {
+      //comparison between adjacent positions in number array
+      //if left one is bigger: swap in both arrays
+      if (parseInt(numsUnsort[i]) > parseInt(numsUnsort[i + 1])) {
+        let auxNum = numsUnsort[i];
+        let auxClass = classUnsort[i];
+        numsUnsort[i] = numsUnsort[i + 1];
+        classUnsort[i] = classUnsort[i + 1];
+        numsUnsort[i + 1] = auxNum;
+        classUnsort[i + 1] = auxClass;
+      }
+      i++; //sorting until whole array is done
+    }
+    wall--; //reducing wall for optimization
+  }
+  //creating cards for every number and respective class
+  for (let i = 0; i < numsUnsort.length; i++) {
+    //in case of 1 11 12 13, turn number into A J Q K
+    let num = numsUnsort[i];
+    switch (num) {
+      case "1":
+        classUnsort[i].innerHTML = "A";
+        break;
+      case "11":
+        classUnsort[i].innerHTML = "J";
+        break;
+      case "12":
+        classUnsort[i].innerHTML = "Q";
+        break;
+      case "13":
+        classUnsort[i].innerHTML = "K";
+        break;
+      default:
+        classUnsort[i].innerHTML = num;
+    }
+    cardSpace.append(classUnsort[i]);
+  }
+});
